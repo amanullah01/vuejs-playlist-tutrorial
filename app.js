eb34b9f655db98f9ee2431f2fe7e5e4e878a8ece
Amanullah Aman <amancse.aiub@gmail.com>
2019-07-11 22:19:39 +0600

    Data binding v-bind

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el: "#vue-app",
   data: {
     name: "Aman",
-    job: "Software Engineer"
+    job: "Software Engineer",
+    website: "https://www.google.com",
+    webTag: "<a href='https://www.google.com' target='_blank'>Google</a>"
   },
   methods: {
     greet(time) {
