7d7d5bef817291aad9525d59da25c49f845204e6
Amanullah Aman <amancse.aiub@gmail.com>
2019-07-11 22:42:48 +0600

    add and subs year in click event

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 new Vue({
   el: "#vue-app",
   data: {
-    age: 29,
+    age: 29
   },
   methods: {
     add(inc) {
-      this.age +=inc;
+      this.age += 1;
     },
     sub(dec){
         this.age -=dec;
