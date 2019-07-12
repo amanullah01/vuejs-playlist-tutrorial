62437bc0ac57e24e53cf597aac104712f8bfa22d
Amanullah Aman <amancse.aiub@gmail.com>
2019-07-12 20:49:40 +0600

    v-for from array and object

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 new Vue({
   el: "#vue-app",
   data: {
-    error: false,
-    success: false
+    characters: ["Amanullah", "Aman", "Suzon", "Khan"],
+    ninjas: [
+      {
+        name: "Amanullah",
+        age: 29
+      },
+      {
+        name: "Aman",
+        age: 30
+      },
+      {
+        name: "Suzon",
+        age: 16
+      },
+      {
+        name: "Khan",
+        age: 14
+      }
+    ]
   },
   methods: {},
   computed: {}
