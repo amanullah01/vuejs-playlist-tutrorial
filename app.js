18b5538f0e96e2d64b0fa10b0856ffeb8587b56c
Amanullah Aman <amancse.aiub@gmail.com>
2019-07-17 12:15:58 +0600

    Introduction to component

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
+Vue.component("greeting", {
+  template:
+    "<p>Hello my name is {{ name }} <button @click='changeMe'>Change me</button></p>",
+  data: function() {
+    return {
+      name: "Amanullah Aman"
+    };
+  },
+  methods: {
+    changeMe: function() {
+      this.name = "Mr. Rafi";
+    }
+  }
+});
+
 var one = new Vue({
   el: "#vue-app-one",
   data: {
     title: "Vue app one title"
   },
   methods: {},
-  computed: {
-    greet: function() {
-      return "Hello, Good Morning app one instance";
-    }
-  }
+  computed: {}
 });
 
 var two = new Vue({
   el: "#vue-app-two",
   data: {
-    title: "Vue app two title",
-    outside: false,
-    outText: ""
+    title: "Vue app two title"
   },
-  methods: {
-    changeOneTitle: function() {
-      one.title = "Changed from vue app two. This is title one";
-    }
-  },
-  computed: {
-    greet: function() {
-      return "Hello, Evening app two instance ";
-    }
-  }
+  methods: {},
+  computed: {}
 });
-
-two.outside = true;
-two.outText = "This text comes from outside on instance";
