0582e85e2b79e6b3cc1b6372d494dbe3eb7559e2
Amanullah Aman <amancse.aiub@gmail.com>
2019-07-17 12:43:33 +0600

    References Refs

--- app.js
+++ app.js
@@ -1,32 +1,17 @@
-Vue.component("greeting", {
-  template:
-    "<p>Hello my name is {{ name }} <button @click='changeMe'>Change me</button></p>",
-  data: function() {
-    return {
-      name: "Amanullah Aman"
-    };
+new Vue({
+  el: "#vue-app",
+  data: {
+    title: "Hello world",
+    favFood: "",
+    divTitle: ""
   },
   methods: {
-    changeMe: function() {
-      this.name = "Mr. Rafi";
+    readRefs: function() {
+      console.log("clicked");
+      console.log(this.$refs);
+      this.favFood = this.$refs.food.value;
+      this.divTitle = this.$refs.div_title.innerText;
     }
-  }
-});
-
-var one = new Vue({
-  el: "#vue-app-one",
-  data: {
-    title: "Vue app one title"
   },
-  methods: {},
-  computed: {}
-});
-
-var two = new Vue({
-  el: "#vue-app-two",
-  data: {
-    title: "Vue app two title"
-  },
-  methods: {},
   computed: {}
 });
