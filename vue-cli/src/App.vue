6a61b1d1a6fbc8eb477bc369c675fddde26321ad
Amanullah Aman <amancse.aiub@gmail.com>
2019-07-23 12:07:09 +0600

    Ninjas component create and call it globally

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>-->
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ greeting() }}</p>
+    <ninjas></ninjas>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      title: "Ninja App"
+    };
+  },
+  methods: {
+    greeting() {
+      return "Good moring!";
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+//globaly import components
+import Ninjas from "./components/Ninjas.vue";
+Vue.component("ninjas", Ninjas);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
