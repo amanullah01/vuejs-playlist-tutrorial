df8b1cf83a357654b303bb630bef7617949af59f
Amanullah Aman <amancse.aiub@gmail.com>
2019-07-23 13:12:04 +0600

    Footer component registerd

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <app-header></app-header>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   components: {
-    "app-header": Header
+    "app-header": Header,
+    "app-footer": Footer
   },
   data: function() {
     return {};
