12c37f8deab745309b19c836b39f4bfce161505d
Amanullah Aman <amancse.aiub@gmail.com>
2019-07-23 12:34:18 +0600

    clean

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,43 +1,16 @@
 <template>
-  <!-- <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>-->
-  <div>
-    <h1>{{ title }}</h1>
-    <p>{{ greeting() }}</p>
-    <ninjas></ninjas>
-  </div>
+  <div></div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-import Ninjas from "./components/Ninjas.vue";
 
 export default {
-  components: {
-    ninjas: Ninjas
-  },
   data: function() {
-    return {
-      title: "Ninja App"
-    };
-  },
-  methods: {
-    greeting() {
-      return "Good moring!";
-    }
+    return {};
   }
 };
 </script>
 
-<style scoped>
-h1 {
-  color: purple;
-}
-</style>
 <style>
-p {
-  color: green;
-}
 </style>
