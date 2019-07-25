4970173cc42795c7d6dcc920256ed3c3be78de5d
Amanullah Aman <amancse.aiub@gmail.com>
2019-07-25 11:45:11 +0600

    form helper, form one and from two created

---
+++ src/components/formOne.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <form-helper>
+      <div slot="form-header">
+        <h3>Form One - Contact Us</h3>
+        <p>Please get in touch.</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="Name" required />
+        <label for>Your message:</label>
+        <textarea name id cols="15" rows="5"></textarea>
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Send</button>
+      </div>
+    </form-helper>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ src/components/formTwo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <form-helper>
+      <div slot="form-header">
+        <h3>Form Two - Login form</h3>
+        <p>Enter your details to logged in</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="Username" required />
+        <input type="password" placeholder="Password" required />
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Login</button>
+      </div>
+    </form-helper>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
